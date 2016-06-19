c5d59af70d78bf09b321ea5a1b733e143ee6fc88
guohongyao <ghongyao@163.com>
2016-06-19 23:55:29 +0800

    添加SD卡缓存

--- app/src/main/java/com/example/guohongyao/imageloader/ImageLoader.java
+++ app/src/main/java/com/example/guohongyao/imageloader/ImageLoader.java
@@ -16,38 +16,52 @@ import java.util.concurrent.Executors;
  * Created by GuoHongyao on 2016/6/18.
  */
 public class ImageLoader {
-    private ImageCache imageCache;
+    //内存缓存
+    private ImageCache imageCache = new ImageCache();
+    //SD卡缓存
+    DiskCache diskCache = new DiskCache();
+    //是否使用SD卡缓存
+    boolean isUseDiskCache = false;
     //线程池，线程数量为CPU的数量
-    private ExecutorService executorService= Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    public ImageLoader() {
-        imageCache=new ImageCache();
-    }
+    private ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+    public ImageLoader() {
+        imageCache = new ImageCache();
+    }
 
 
     public void displayImage(final String imageUrl, final ImageView imageView) {
         imageView.setTag(imageUrl);
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                Bitmap bitmap=downloadImage(imageUrl);
-                if(bitmap==null){
-                    return;
+        Bitmap bitmap = isUseDiskCache ? diskCache.get(imageUrl) : imageCache.get(imageUrl);
+        if(bitmap==null) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    Bitmap bitmap = downloadImage(imageUrl);
+                    if (bitmap == null) {
+                        return;
+                    }
+                    showImage(bitmap, imageView, imageUrl);
                 }
-                if(imageView.getTag()==imageUrl){
-                    imageView.setImageBitmap(bitmap);
-                }
-                imageCache.put(imageUrl,bitmap);
-            }
-        });
+            });
+        }else{
+            showImage(bitmap, imageView, imageUrl);
+        }
+    }
+
+    private void showImage(Bitmap bitmap, ImageView imageView, String imageUrl) {
+        if (imageView.getTag() == imageUrl) {
+            imageView.setImageBitmap(bitmap);
+        }
+        imageCache.put(imageUrl, bitmap);
     }
 
     public Bitmap downloadImage(String imageUrl) {
-        Bitmap bitmap=null;
+        Bitmap bitmap = null;
         try {
-            URL url=new URL(imageUrl);
-            HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
-            bitmap= BitmapFactory.decodeStream(httpURLConnection.getInputStream());
+            URL url = new URL(imageUrl);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(httpURLConnection.getInputStream());
             httpURLConnection.disconnect();
         } catch (MalformedURLException e) {
             e.printStackTrace();
